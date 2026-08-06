6b61eeca17b28c14761d620c9257b6047262a0a9
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2026-08-06 15:49:21 +0700

    fix build

--- src/test/java/cc/kertaskerja/manrisk/ManriskApplicationTests.java
+++ src/test/java/cc/kertaskerja/manrisk/ManriskApplicationTests.java
@@ -2,10 +2,14 @@ package cc.kertaskerja.manrisk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@Import(TestContainersConfiguration.class)
+@SpringBootTest()
+@ActiveProfiles("test")
 class ManriskApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
